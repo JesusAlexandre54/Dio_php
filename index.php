26458b7455aeee0968f8d63469e3440004384536
Alexandre Jesus <alexandrej.engenharia@gmail.com>
2021-09-12 16:02:05 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+</head>
+<body>
+    <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIÇÕES</p>
+    <form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>  
+    <p><input type="submit"></p> 
+    </form>
+    
+</body>
+</html>
